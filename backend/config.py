256080f714d0bc5ccfcf262864defd69f627c59a
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 14:25:37 +0200

    wrapped app to CORS

--- backend/config.py
+++ backend/config.py
@@ -2,3 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+#Our server can't be hit from a different URL. Also need to connect frontend with backend
+
+app = Flask(__name__) #Next, wrap out app into CORS
+CORS(app)
+
+
