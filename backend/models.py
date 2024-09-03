8d7416a396e10da021c83196c251e9a3bf10fbcb
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 14:48:58 +0200

    set json deff to convert db into python

--- backend/models.py
+++ backend/models.py
@@ -7,3 +7,11 @@ class Contact(db.Model):
     first_name = db.Column(db.String(80), unique=False, nullable=False)
     last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_json(self): #JSON takes all of the fields we have in contact and converts it to Python dictionary and we can pass from out API
+        return {
+            "id": self.id,
+            "firstName": self.last_name, #camel case used for JSON, word & Capital lett.word
+            "lastName": self.last_name,
+            "email": self.email,
+        }
