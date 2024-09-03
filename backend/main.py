bfb4c921a1b0dd3ee6223a5ff141b0e248ab79f9
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 16:02:32 +0200

    created python class and added to db session corresponding to the entry

--- backend/main.py
+++ backend/main.py
@@ -38,6 +38,11 @@ def create_contact():
         )
     
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
 if __name__ == "__main__":
     with app.app_context():
