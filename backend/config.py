6c0ebf6c27502aba368da5a6200d5267bc3b6b60
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 14:29:34 +0200

    Specified the location of the local SQL where we store our machine

--- backend/config.py
+++ backend/config.py
@@ -7,4 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__) #Next, wrap out app into CORS
 CORS(app)
 
+#Initialize database things
+#Specifying the location of the local SQL where we store our machine
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
 
