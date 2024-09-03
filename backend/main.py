0c8312e78867f333675af1a34dc3019f2497bf76
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 16:05:36 +0200

    created message 201 (after building new contact)

--- backend/main.py
+++ backend/main.py
@@ -43,6 +43,8 @@ def create_contact():
         db.session.commit()
     except Exception as e:
         return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "User created!"}), 201
 
 if __name__ == "__main__":
     with app.app_context():
