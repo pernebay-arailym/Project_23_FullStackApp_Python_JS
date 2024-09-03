d24a3c0a76a4b69cf5eda8ef7f26bbd2731f3e70
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 14:41:11 +0200

    created class Contact

--- backend/models.py
+++ backend/models.py
@@ -4,4 +4,6 @@ from config import db
 class Contact(db.Model):
     id = db.Column(db.Integer, primery_key=True)
     #this is a key that we use to index, must be unique for every single entry of the database
-    
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
