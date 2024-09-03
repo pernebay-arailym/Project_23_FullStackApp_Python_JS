449db9844b125de755746efc9d21a088dbddbd99
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-03 15:26:25 +0200

    speed up the db & query all contacts to have a list

--- backend/main.py
+++ backend/main.py
@@ -19,4 +19,16 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all() #gives a list of all contacts
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all() #speed up the db
+
+
+    app.run(debug=True)
+
 
