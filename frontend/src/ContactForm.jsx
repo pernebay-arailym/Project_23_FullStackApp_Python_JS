b5fda4e99e39b21c98d55987101ab95aab48ec47
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-04 16:07:59 +0200

    set submit function & exported the form

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,14 +1,33 @@
 import {useState} from "react"
 
 const ContactForm = ({}) => {
-    const [firstName, setFirstName] = useState("")
-    const [lastname, setLastName] = useState("")
-    const [email, setEmail] = useState("")
+    const [firstName, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
 
-    return <form>
-        <div>
-            <label htmlFor="firstName">First Name:</label>
-            <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-        </div>
-    </form>
-}
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <button type="submit">Create Contact</button>
+        </form>
+    );
+
+};
+
+export default ContactForm
