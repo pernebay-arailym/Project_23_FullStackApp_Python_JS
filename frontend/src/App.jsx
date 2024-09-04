316b0464dfa7f74e877fe64adc2a6462e7750dcd
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-04 16:44:13 +0200

    imported ContactForm so that to get all created contacts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import { useState, useEffect } from 'react'
-import Contactlist from './ContactList'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Contactlist from './ContactList';
+import './App.css';
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     fetchContacts()
-  }, [])
+  }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
-  }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+    console.log(data.contacts);
+  };
 
 
   return (
     <>
-      
+    <Contactlist contacts={contacts}/>
+    <ContactForm/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
