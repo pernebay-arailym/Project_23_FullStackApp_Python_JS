9d43a00b3c16dc93a5a01c3fe75ddc183e5db684
pernebay-arailym <pernebayarailym@gmail.com>
2024-09-04 16:31:07 +0200

    added objects to Submit function & converted to JSON

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -8,6 +8,20 @@ const ContactForm = ({}) => {
     const onSubmit = (e) => {
         e.preventDefault()
 
+        const data = {
+            firstName,
+            lastName,
+            email
+        }
+        const url = "http://127.0.0.1:5000/create_contact"
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data) 
+        }
+
     }
 
     return (
